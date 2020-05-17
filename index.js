1cf2a1c192434aad2ce5ee04da1e89ab36ef5e6f
Borys Koretskyi <borys.koretskyi@gmail.com>
2020-05-17 17:14:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+export const createDebouncePromise = (func, wait) =>
+  new Promise((res, rej) =>
+    setTimeout(() => {
+      try {
+        const result = func()
+        res(result)
+      } catch (e) {
+        rej(e)
+      }
+    }, wait)
+  )
+
+export default function clientMiddleware({ client, handleError }) {
+  return ({ dispatch, getState }) => {
+    return next => action => {
+      if (typeof action === 'function') {
+        return action(dispatch, getState)
+      }
+
+      const { promise, types, debounce, ...rest } = action
+      if (!promise) {
+        return next(action)
+      }
+
+      const [REQUEST, SUCCESS, FAILURE] = types
+      next({ ...rest, type: REQUEST })
+
+      const actionPromise = debounce
+        ? createDebouncePromise(() => promise(client), debounce)
+        : promise(client)
+
+      actionPromise
+        .then(result => next({ ...rest, result, type: SUCCESS }))
+        .catch(error => {
+          handleError(error)
+
+          next({ ...rest, error, type: FAILURE })
+        })
+
+      return actionPromise
+    }
+  }
+}
